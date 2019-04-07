1d40de79dc5e4c7e5fe07e752d23d13c780e139d
Igor <zxvnme@protonmail.com>
2019-04-07 02:06:42 +0200

    [gamesense] more_indicators lua update

--- gamesense/more_indicators.lua
+++ gamesense/more_indicators.lua
@@ -2,6 +2,7 @@ local more_indicators_control = ui.new_checkbox("VISUALS", "Other ESP", "More in
 
 local force_bodyaim_control = ui.reference("RAGE", "Other", "Force body aim")
 local double_tap_control = ui.reference("RAGE", "Other", "Double tap")
+local on_shot_aa_control = ui.reference("AA", "Other", "On shot anti-aim")
 
 local screen_size_x, screen_size_y = client.screen_size()
 
@@ -16,7 +17,11 @@ local function on_paint(ctx)
 	end
 
 	if ui.get(double_tap_control) then
-		client.draw_indicator(ctx, 39, 174, 96, 255, "2x TAP")
+		client.draw_indicator(ctx, 39, 174, 96, 255, "DOUBLE TAP")
+	end
+
+	if ui.get(on_shot_aa_control) then
+		client.draw_indicator(ctx, 39, 174, 96, 255, "ON SHOT AA")
 	end
 end
 
