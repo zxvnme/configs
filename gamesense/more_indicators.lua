ca9af43b0f73777460a662e8622048c71a748373
Igor <zxvnme@protonmail.com>
2019-04-08 18:35:25 +0200

    [gamesense] more_indicators.lua improvements

--- gamesense/more_indicators.lua
+++ gamesense/more_indicators.lua
@@ -1,28 +1,34 @@
-local more_indicators_control = ui.new_checkbox("VISUALS", "Other ESP", "More indicators")
+-- api methods
+local ui_get, ui_reference, ui_new_checkbox, client_draw_indicator, client_screen_size, client_set_event_callback = 
+      ui.get, ui.reference, ui.new_checkbox, client.draw_indicator, client.screen_size, client.set_event_callback
 
-local force_bodyaim_control = ui.reference("RAGE", "Other", "Force body aim")
-local double_tap_control = ui.reference("RAGE", "Other", "Double tap")
-local on_shot_aa_control = ui.reference("AA", "Other", "On shot anti-aim")
+-- new elements
+local more_indicators_control = ui_new_checkbox("VISUALS", "Other ESP", "More indicators")
+-- references
+local force_bodyaim_control = ui_reference("RAGE", "Other", "Force body aim")
+local double_tap_control = ui_reference("RAGE", "Other", "Double tap")
+local on_shot_aa_control = ui_reference("AA", "Other", "On shot anti-aim")
+-- vars
+local screen_size_x, screen_size_y = client_screen_size()
 
-local screen_size_x, screen_size_y = client.screen_size()
-
+-- paint event callback
 local function on_paint(ctx)
-	if not ui.get(more_indicators_control) then
+	if not ui_get(more_indicators_control) then
 		return
 	end
 
-	if ui.get(force_bodyaim_control) then
-		client.draw_indicator(ctx, 39, 174, 96, 255, "FORCE BAIM")
-
+	if ui_get(force_bodyaim_control) then
+		client_draw_indicator(ctx, 39, 174, 96, 255, "FORCE BAIM")
 	end
 
-	if ui.get(double_tap_control) then
-		client.draw_indicator(ctx, 39, 174, 96, 255, "DOUBLE TAP")
+	if ui_get(double_tap_control) then
+		client_draw_indicator(ctx, 39, 174, 96, 255, "DOUBLE TAP")
 	end
 
-	if ui.get(on_shot_aa_control) then
-		client.draw_indicator(ctx, 39, 174, 96, 255, "ON SHOT AA")
+	if ui_get(on_shot_aa_control) then
+		client_draw_indicator(ctx, 39, 174, 96, 255, "ON SHOT AA")
 	end
 end
 
-client.set_event_callback("paint", on_paint)
+-- register callbacks
+client_set_event_callback("paint", on_paint)
